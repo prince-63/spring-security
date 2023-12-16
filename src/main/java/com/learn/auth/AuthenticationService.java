49040a8e2b6191cfbb17844e02af61bbfe5a16e3
Prince <princekumar.priku@gmail.com>
2023-12-16 14:59:15 +0530

    Initial commit

---
+++ src/main/java/com/learn/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.learn.auth;
+
+import com.learn.config.JwtService;
+import com.learn.user.Role;
+import com.learn.user.User;
+import com.learn.user.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+    private final UserRepo userRepo;
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authentication(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepo.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+}
---
+++ src/main/java/com/learn/user/Role.java
@@ -0,0 +1,6 @@
+package com.learn.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
